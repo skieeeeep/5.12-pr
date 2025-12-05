2767963d08eb2d3bdc868fdd12f7c808646798d4
Skip <89522640535e@gmail.com>
2025-12-05 21:25:17 +0300

    finished, i guess

--- 5.12 pr/5.12 pr.cpp
+++ 5.12 pr/5.12 pr.cpp
@@ -145,3 +145,22 @@ topit::rect::rect(p_t u_l, p_t r_b):
 topit::p_t topit::rect::begin() const {
 	return upper_left;
 }
+
+topit::p_t topit::rect::next(p_t prev) const {
+	if (prev.x == upper_left.x && prev.y > right_bottom.y) {
+		return { prev.x, prev.y - 1 };
+	}
+	else if (prev.x < right_bottom.x && prev.y == right_bottom.y) {
+		return { prev.x + 1, prev.y };
+	}
+	else if (prev.x == right_bottom.x && prev.y < upper_left.y) {
+		return { prev.x, prev.y + 1 };
+	}
+	else if (prev.x > upper_left.x && prev.y == upper_left.y) {
+		return { prev.x - 1, prev.y };
+	}
+	else if (prev.x == upper_left.x && prev.y == upper_left.y) {
+		return upper_left;
+	}
+	throw std::logic_error("bad impl");
+}
