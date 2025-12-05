b3eb636085380b039dc234ed5322e3503e6685b4
skip <89522640535egmail.com>
2025-12-05 12:44:40 +0300

    Dot

--- 5.12 pr/5.12 pr.cpp
+++ 5.12 pr/5.12 pr.cpp
@@ -2,9 +2,38 @@
 
 #include <iostream>
 
+namespace topit {
+	struct p_t {
+		int x, y;
+	};
+
+	bool operator==(p_t, p_t);
+	bool operator!=(p_t, p_t);
+	struct f_t { 
+		p_t aa, bb;
+	};
+
+	struct IDraw {
+		virtual ~IDraw() = default;
+		virtual p_t begin() const = 0;
+		virtual p_t next(p_t) const = 0;
+	};
+
+	struct Dot : IDraw {
+		p_t next() const override;
+		p_t next(p_t) const override;
+	};
+}
 int main()
 {
-   
+	using topit::p_t;
+	p_t a{ 1,0 }, b{ 1,0 };
+	std::cout << (a != b) << "\n";
 }
 
-
+bool topit:: operator==(p_t a, p_t b) {
+	return a.x == b.x && a.y == b.y;
+}
+bool topit:: operator!=(p_t a, p_t b) {
+	return a.x != b.x && a.x != b.y;
+}
